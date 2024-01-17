cc839b5285560c30a773802bb39a4dfa1e6f3e96
Barry Allahyar <barrya@gmail.com>
2024-01-17 12:21:20 -0600

    Create routes for airlines and flights.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :airlines, only: [:index, :show]
+  resources :flights, only: [:show]
 end
